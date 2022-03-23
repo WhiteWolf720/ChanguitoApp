50a6406cdb74599f40c3c0bbbd4d7e5829d91919
WhiteWolf720 <diegooy.ti20@utsjr.edu.mx>
2022-03-23 13:21:22 -0600

    2do envio

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -10,46 +10,82 @@ class HomePage extends StatelessWidget
     (
       body:Center
       (
-        child:Container
+        child: Container
         (
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('lib/src/img/fondo.png')
-          ),
-        ),
-        child: Column(
-          children: <Widget>[
-            Stack(
-              children: <Widget>[
-                Image.asset('lib/src/img/gradient.png'),
-                Image.asset('lib/src/img/face.png')
-
-              ]),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const SizedBox(width: 5.0),
-                Text('Changito', 
-                style: TextStyle(
-                  fontSize: 33.0,
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).primaryColor
-                ),
-                ),
-                const SizedBox(width: 5.0),
-                Text('Delivery', 
-                style: TextStyle(
-                  fontSize: 33.0,
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).primaryColor
-                )
-                )
-              ],
+          alignment: AlignmentDirectional.center,
+          decoration: const BoxDecoration
+          (
+            image: DecorationImage
+            (
+              fit: BoxFit.cover,
+              image: AssetImage('lib/src/images/fondo.png'),
+              opacity: 0.2
             )
-          ]),
-      )
+          ),
+          child: Column
+          (
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>
+            [
+              Stack
+              (
+                alignment: AlignmentDirectional.center,
+                children: <Widget>
+                [
+                  Image.asset
+                  (
+                    'lib/src/images/gradient.png',
+                    width: 100.0
+                  ),
+                  Image.asset
+                  (
+                    'lib/src/images/face.png',
+                  )
+                ]
+              ),
+              Row
+              (
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>
+                [
+                  Text
+                  (
+                    "Meal",
+                    style: TextStyle
+                    (
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor
+                    )
+                  ),
+                  const SizedBox
+                  (
+                    width: 5.0,
+                  ),
+                  Text
+                  (
+                    "Monkey",
+                    style: TextStyle
+                    (
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.secondary
+                    )
+                  )
+                ],
+              ),
+              const SizedBox
+              (
+                height: 10.0
+              ),
+              const Text
+              (
+                "FOOD DELIVERY"
+              )
+            ]
+          )
+        ),
       )
     );
   }
 }
-
