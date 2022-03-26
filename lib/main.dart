5550618f5c91598dbbe0fc8ea376da1242d2b456
WhiteWolf720 <diegooy.ti20@utsjr.edu.mx>
2022-03-26 09:44:08 -0600

    4to envio - Login

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,14 @@ void main() {
     MaterialApp
     (
       debugShowCheckedModeBanner: false,
-      title: 'Monkey Delivery',
-      initialRoute: '/',
+      title: 'Changuito Express',
+      initialRoute: '/login',
       routes: routes,
       theme: ThemeData(
         primaryColor: Color.fromRGBO(252, 96, 17, 1.0),
         colorScheme: ColorScheme.fromSwatch().copyWith(
           secondary: const Color.fromRGBO(74, 75, 77, 1.0)
           )
-        //primarySwatch: Colors.blue,
       ),
     )
   );
--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,11 +1,10 @@
 import 'package:changutiapp/main.dart';
 import 'package:changutiapp/pages/loginPage.dart';
+import 'package:changutiapp/pages/homePage.dart';
 import 'package:flutter/cupertino.dart';
-
-import '../pages/homePage.dart';
 
 final routes=<String, WidgetBuilder>
 {
   '/':(BuildContext context)=>const HomePage(),
-  '/login':(BuildContext context) => const loginPage()
+  '/login':(BuildContext context) => const LoginPage()
 };
