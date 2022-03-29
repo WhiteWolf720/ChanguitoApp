f26089aaddf470b1950faecd0cc73a1f140d56a5
WhiteWolf720 <diegooy.ti20@utsjr.edu.mx>
2022-03-29 15:07:38 -0600

    6to envio - Terminado

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -51,14 +51,14 @@ class LoginPage extends StatelessWidget
                      child: Wrap(
                        alignment: WrapAlignment.center,
                        children: <Widget>[
-                        Text('Sopa de macaco',
+                        Text('Sopa de macaco, uma delicia',
                         style: TextStyle(
                           color: Color.fromRGBO(124, 125, 126, 1.0),
                           fontSize: 18.0
                         ),
                         ),
                         SizedBox(height: 25.0,),
-                        Text('Uma delicia',
+                        Text('soloh agi om Meal Monkey',
                         style: TextStyle(
                           color: Color.fromRGBO(124, 125, 126, 1.0),
                           fontSize: 18.0
@@ -76,12 +76,105 @@ class LoginPage extends StatelessWidget
                        borderRadius: BorderRadius.circular(30),
                        color: Theme.of(context).primaryColor
                      ),
+                     child: const TextButton(
+                       onPressed: null,
+                       child: Text('Login',
+                       style: TextStyle(
+                         fontSize: 18.0,
+                         color: Colors.white
+                        )
+                       ),
+                     ),
+                   ),
+                   const SizedBox(height: 10.0),
+                   Container(
+                     width: double.infinity,
+                     height: 55.0,
+                     margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+                     decoration: BoxDecoration(
+                       borderRadius: BorderRadius.circular(30),
+                       border: Border.all(
+                         color: Theme.of(context).primaryColor,
+                       )
+                       //color: Theme.of(context).primaryColor
+                     ),
+                     child: const TextButton(
+                       onPressed: null,
+                       child: Text('Crea una cuenta, chango',
+                       style: TextStyle(
+                         fontSize: 18.0,
+                         color: Colors.orange
+                        )
+                       ),
+                     ),
                    )
                  ],
                ),
              )
             ]
-         )
+         ),
+         Column
+          (
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>
+            [
+              Stack
+              (
+                alignment: AlignmentDirectional.center,
+                children: <Widget>
+                [
+                  Image.asset
+                  (
+                    'lib/src/images/gradient.png',
+                    width: 100.0
+                  ),
+                  Image.asset
+                  (
+                    'lib/src/images/face.png',
+                  )
+                ]
+              ),
+              Row
+              (
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>
+                [
+                  Text
+                  (
+                    "Meal",
+                    style: TextStyle
+                    (
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor
+                    )
+                  ),
+                  const SizedBox
+                  (
+                    width: 5.0,
+                  ),
+                  Text
+                  (
+                    "Monkey",
+                    style: TextStyle
+                    (
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.secondary
+                    )
+                  )
+                ],
+              ),
+              const SizedBox
+              (
+                height: 10.0
+              ),
+              const Text
+              (
+                "FOOD DELIVERY"
+              )
+            ]
+          )
         ]
       )
     );
