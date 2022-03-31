90ab1b910c8be2a3ada3961486b4130501f7a4ad
WhiteWolf720 <diegooy.ti20@utsjr.edu.mx>
2022-03-31 09:24:34 -0600

    10mo envio - NewPassword

--- lib/pages/loginPassword.dart
+++ lib/pages/loginPassword.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
-class ResetCode extends StatelessWidget {
-  const ResetCode({Key? key}) : super(key: key);
-
+class LoginPassword extends StatelessWidget {
+  const LoginPassword({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     final _height = MediaQuery.of(context).size.height;
-
     return Scaffold(
       body: Stack(
         children: <Widget>[
@@ -24,57 +23,76 @@ class ResetCode extends StatelessWidget {
                       alignment: WrapAlignment.center,
                       children: [
                         const Text(
-                          'Hemos enviado un OTP a',
+                          'New Password',
                           style: TextStyle(
                               color: Color.fromRGBO(124, 125, 126, 1.0),
-                              fontSize: 25.0),
-                        ),
-                        const Text(
-                          'tu dispositivo movil',
-                          style: TextStyle(
-                              color: Color.fromRGBO(124, 125, 126, 1.0),
-                              fontSize: 25.0),
-                        ),
-                        const SizedBox(
-                          height: 50.0,
-                        ),
-                        const Text(
-                          'Por favor revisa tu movil 427*****12',
-                          style: TextStyle(
-                              color: Color.fromRGBO(124, 125, 126, 1.0),
-                              fontSize: 14.0),
-                        ),
-                        const Text(
-                          'para poder reiniciar tu contraseña',
-                          style: TextStyle(
-                              color: Color.fromRGBO(124, 125, 126, 1.0),
-                              fontSize: 14.0),
-                        ),
-                        const SizedBox(
-                          height: 75.0,
+                              fontSize: 35.0),
                         ),
                         const SizedBox(
                           height: 30.0,
                         ),
+                        const Text(
+                          'Por favor ingresa una nueva contraseña',
+                          style: TextStyle(
+                              color: Color.fromRGBO(124, 125, 126, 1.0),
+                              fontSize: 18.0),
+                        ),
+                        const Text(
+                          'y a continuacion confirma la nueva contraseña',
+                          style: TextStyle(
+                              color: Color.fromRGBO(124, 125, 126, 1.0),
+                              fontSize: 18.0),
+                        ),
+                        const SizedBox(
+                          height: 75.0,
+                        ),
                         Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 45.0),
-                          margin: const EdgeInsets.symmetric(horizontal: 45.0),
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          margin: const EdgeInsets.symmetric(horizontal: 20.0),
                           decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(25.0),
                               border: Border.all(color: Colors.grey)),
-                          child: const TextField(
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  color: Color.fromRGBO(124, 125, 126, 1.0),
-                                  fontSize: 40.0),
-                              decoration: InputDecoration(
-                                  //prefixIcon: Icon(Icons.person_outline),
-                                  //labelText: '*   *   *   *',
-                                  hintText: '*   *   *   *')),
+                          child: TextField(
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                  prefixIcon: Icon(Icons.lock),
+                                  labelText: 'Nueva Contraseña',
+                                  hintText: 'BaNaNaS69')),
                         ),
                         const SizedBox(
-                          height: 90.0,
+                          height: 30.0,
                         ),
+                        const SizedBox(
+                          height: 80.0,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          margin: const EdgeInsets.symmetric(horizontal: 20.0),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25.0),
+                              border: Border.all(color: Colors.grey)),
+                          child: TextField(
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                  prefixIcon: Icon(Icons.lock),
+                                  labelText: 'Confirma Nueva Contraseña',
+                                  hintText: 'BaNaNaS69')),
+                        ),
+                      ],
+                    ),
+                  ),
+                ]),
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: _height * 0.5,
+                child: Column(children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: Wrap(
+                      alignment: WrapAlignment.center,
+                      children: <Widget>[
+                        //const SizedBox(height: 30.0),
                         Container(
                           width: double.infinity,
                           height: 55.0,
@@ -85,7 +103,7 @@ class ResetCode extends StatelessWidget {
                               color: Theme.of(context).primaryColor),
                           child: const TextButton(
                             onPressed: null,
-                            child: Text('Send',
+                            child: Text('Siguiente',
                                 style: TextStyle(
                                     fontSize: 18.0, color: Colors.white)),
                           ),
@@ -93,7 +111,6 @@ class ResetCode extends StatelessWidget {
                       ],
                     ),
                   )
-                  
                 ]),
               )
             ],
--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -3,6 +3,7 @@ import 'package:changutiapp/pages/loginCode.dart';
 import 'package:changutiapp/pages/loginMethods.dart';
 import 'package:changutiapp/pages/loginPage.dart';
 import 'package:changutiapp/pages/homePage.dart';
+import 'package:changutiapp/pages/loginPassword.dart';
 import 'package:changutiapp/pages/loginReset.dart';
 import 'package:changutiapp/pages/loginSignUp.dart';
 import 'package:flutter/cupertino.dart';
@@ -14,5 +15,6 @@ final routes=<String, WidgetBuilder>
   '/logins':(BuildContext context) => const LoginMethods(),
   '/signup':(BuildContext context) => const SignUp(),
   '/resetpass':(BuildContext context) => const ResetPass(),
-  '/resetcode':(BuildContext context) => const ResetCode()
+  '/resetcode':(BuildContext context) => const ResetCode(),
+  '/newpass':(BuildContext context) => const LoginPassword()
 };
